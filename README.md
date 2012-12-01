485a4d94f185092c8d1b1804e0aa3a2c82e90028
Tzach Solomon <tzach.solomon@gmail.com>
2012-12-01 20:52:04 +0200

    1. Added an example how to check the application is visible to the user
    Credit goes to http://commonsware.com/blog/2010/08/11/activity-notification-ordered-broadcast.html for the idea

--- README.md
+++ README.md
@@ -3,4 +3,8 @@ The example has 2 buttons:
 1. Press on the first button and both receivers will accpet the broadcast
 2. Press on the second button and the first receiver will abort the broadcast, thus it will not pass to the 2nd receiver
 
+When checking the 1st Check Box (Register Dynamic broadcast), it will register a dynamic broadcast receiver with priority 3.
+This means that the dynamic receiver will be triggered before the other static receivers.
+when checking on the 2nd Check Box (Abort static broadcast...), it will case the dynamic receiever to the abort the broadcast. This way you can check if you app is visibile to the user (place the register unregister broadcast in the onStart/onStop or onResume/onPause of you app)
+
 Notice: the priority in the AndroidManifest.xml file for each broadcast
--- src/tzach/example/exampleorderedbroadcast/MainActivity.java
+++ src/tzach/example/exampleorderedbroadcast/MainActivity.java
@@ -2,11 +2,16 @@ package tzach.example.exampleorderedbroadcast;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 public class MainActivity extends Activity implements OnClickListener {
 
@@ -14,6 +19,11 @@ public class MainActivity extends Activity implements OnClickListener {
 	private static String ACTION1= "tzach.example.exampleorderedbroadcast.action1";
 	private Button buttonSendBroadcast1;
 	private Button buttonSendBroadcast2;
+	private BroadcastReceiver mBroadcastReceiver;
+	private CheckBox checkBoxBroadcastActivity;
+	private Button buttonBroadcastActivityAbort;
+	private CheckBox checkBoxBroadcastActivityRegister;
+	
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -26,7 +36,41 @@ public class MainActivity extends Activity implements OnClickListener {
         buttonSendBroadcast2 = (Button)findViewById(R.id.buttonSendBroadcast2);
         buttonSendBroadcast2.setOnClickListener(this);
         
+        checkBoxBroadcastActivity = (CheckBox)findViewById(R.id.checkBoxBroadcastActivity);
+        checkBoxBroadcastActivity.setOnClickListener(this);
         
+        checkBoxBroadcastActivityRegister = (CheckBox)findViewById(R.id.checkBoxBroadcastActivityRegister);
+        checkBoxBroadcastActivityRegister.setOnClickListener(this);
+        
+        mBroadcastReceiver = new BroadcastReceiver() {
+			
+			@Override
+			public void onReceive(Context context, Intent intent) {
+				// 
+				Bundle extras = intent.getExtras();
+				
+				Toast.makeText(context, "Dynamic Broadcast from activity", Toast.LENGTH_LONG).show();
+				if (extras.getBoolean("abort1")){
+					abortBroadcast();
+					Toast.makeText(context, "Other (static) broadcasts have been aborted", Toast.LENGTH_LONG).show();
+				}
+				
+			}
+		};
+		
+			
+		
+		
+        
+        
+    }
+    
+    @Override
+    protected void onResume() {
+    	// 
+    	super.onResume();
+    	
+    	
     }
 
     @Override
@@ -43,8 +87,14 @@ public class MainActivity extends Activity implements OnClickListener {
 		case R.id.buttonSendBroadcast1:
 			
 	        intent.setAction(ACTION1);
-	        
+	        if (checkBoxBroadcastActivity.isChecked()){
+	        	intent.putExtra("abort1",true);
+	        }else{
+	        intent.putExtra("abort1",false);
+	        }
 	        intent.putExtra("abort",false);
+	        
+	        
 	        //intent.setClass(getApplicationContext(), MyReceiver1.class);
 	        sendOrderedBroadcast(intent, null);
 
@@ -52,12 +102,29 @@ public class MainActivity extends Activity implements OnClickListener {
 		case R.id.buttonSendBroadcast2:
 			
 	        intent.setAction(ACTION1);
-	        
 	        intent.putExtra("abort",true);
+	        if (checkBoxBroadcastActivity.isChecked()){
+	        	intent.putExtra("abort1",true);
+	        }else{
+	        intent.putExtra("abort1",false);
+	        }
+	        
 	        //intent.setClass(getApplicationContext(), MyReceiver1.class);
 	        
 	        sendOrderedBroadcast(intent, null);
 
+			break;
+		
+		case R.id.checkBoxBroadcastActivityRegister:
+			if (checkBoxBroadcastActivityRegister.isChecked()){
+				IntentFilter filter = new IntentFilter(ACTION1);
+		    	filter.setPriority(3);
+		    	registerReceiver(mBroadcastReceiver, filter);
+		    	Toast.makeText(MainActivity.this, "Registered dynamic broadcast in activity", Toast.LENGTH_LONG).show();
+			}else{
+				unregisterReceiver(mBroadcastReceiver);
+				Toast.makeText(MainActivity.this, "Unregistered dynamic broadcast in activity", Toast.LENGTH_LONG).show();
+			}
 			break;
 		}
 		
